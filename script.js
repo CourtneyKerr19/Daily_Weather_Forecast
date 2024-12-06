aa4180b2e668a1c919e04768a1959533e521979a
Courtney Kerr <courtneykerr219@gmail.com>
2024-12-06 05:56:52 -0600

    Reimagined script.js file; Incorporated async function to fetch weather data from the API.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const apiKey = '61c3c3b2ea79768d54718ba82bbcbf6d';
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 
-function getWeather() {
-  const cityName = document.getElementById('city').value;
+async function getWeather(city) {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+    const data = await response.json();
+    displayWeather(data);
 
-  if(!cityName) {
-    alert('Please enter a city name');
-    return;
+    const hourlyResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
+
+    const hourlyData = await hourlyResponse.json();
+    displayHourlyWeather(hourlyData.list.slice(0, 5));
+  } catch (error) {
+    console.log('Error fetching data', error);
   }
-}
+  }
