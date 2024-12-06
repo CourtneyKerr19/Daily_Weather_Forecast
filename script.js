4f9df6149e0a73d244a50027766f4cbc42bca057
Courtney Kerr <courtneykerr219@gmail.com>
2024-12-06 05:50:54 -0600

    Declared api key and url outside of the fetchWeather function.

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-function getWeather() {
-  const apiKey = '61c3c3b2ea79768d54718ba82bbcbf6d';
-  const city = document.getElementById('city').value;
-}
+const apiKey = '61c3c3b2ea79768d54718ba82bbcbf6d';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
